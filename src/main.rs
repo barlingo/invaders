c8b28bdce84d3d8e50219ec4d690574e976b1657
barlingo <16936512+barlingo@users.noreply.github.com>
2022-07-03 23:18:22 -0400

    added invaders

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
         // Draw and render
         // Ignore first failures
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -81,6 +84,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+        // win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+        }
     }
 
     drop(render_tx);
